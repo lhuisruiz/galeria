3397e1a58ea10018d29d053521df005de5c2e7e3
lhuisruiz <84159346+lhuisruiz@users.noreply.github.com>
2022-03-18 22:20:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,236 @@
+const showBtn = document.querySelector('.navBtn');
+const topNav = document.querySelector('.top-nav');
+
+showBtn.addEventListener('click', function(){
+    if(topNav.classList.contains('showNav')){
+        topNav.classList.remove('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-bars"></i>';
+    } else {
+        topNav.classList.add('showNav');
+        showBtn.innerHTML = '<i class = "fas fa-times"></i>';
+    }
+});
+
+
+
+// ==== INSTAGRAM ======
+
+
+// luchi
+function abrirNuevoTab(url) {
+    
+    var win = window.open(url, '_blank');
+    
+    win.focus();
+  }
+  $('#luchiinst').click(function(){
+    abrirNuevoTab('https://www.instagram.com/luisiana.dark')
+  })
+
+// fran
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#franinst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/franco.rios13/') 
+})
+
+// juan moreno
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#juaninst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/juanmoreno_darc/')
+})
+
+// carlitos
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#carlitosinst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/carlitoos_alvarez/')
+})
+
+// carlitos
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#nikoinst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/nikiaquinoo/')
+})
+
+//juanca
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#juancainst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/juancachoque/')
+})
+
+//niko
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#nikolinst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/nikiaquinoo/')
+})
+
+//fer arias
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#ferinst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/arias.fer22/')
+})
+
+// ara
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#arainst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/nana_da_silva_1231/')
+})
+
+// eve
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#eveinst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/ev_3lin/')
+})
+
+// wiliam
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#wiliaminst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/wiliams.brizuela.723/')
+})
+
+// margarita
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#margaritainst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/marvillalba13/')
+})
+
+// celso
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#celsoinst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/celso_reall/')
+})
+
+// seva
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#sebainst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/sebas_aguilar723/')
+})
+
+// lauty
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#lautyinst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/_.ezequielito_723/')
+})
+
+// valen
+function abrirNuevoTab(url) {
+  
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#valeninst').click(function(){
+  abrirNuevoTab('https://www.instagram.com/valentinfalconefs/')
+})
+
+
+
+
+// ===== VER MAS ====
+
+// luchi
+  function abrirNuevoTab(url) {
+ 
+    var win = window.open(url, '_blank');
+  
+    win.focus();
+  }
+  $('#luchimasfotos').click(function(){
+    abrirNuevoTab('https://lhuisruiz.github.io/lu/')
+  })
+
+// fran
+function abrirNuevoTab(url) {
+ 
+  var win = window.open(url, '_blank');
+
+  win.focus();
+}
+$('#franmasfotos').click(function(){
+  abrirNuevoTab('https://lhuisruiz.github.io/fran/')
+})
+
+
+
+
+/* Lightbox */
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+
+
+/*fran insta
+let instfran = document.getElementById("instfran");
+instfran.addEventListener("click",()=>{
+    location.href="https://www.instagram.com/franco.rios13";
+})*/
+
+
